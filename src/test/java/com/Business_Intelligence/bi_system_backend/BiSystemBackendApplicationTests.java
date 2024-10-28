25972dca6a154d437a320ce38048065c3c978042
Hugo <h.ferreira147@gmail.com>
2024-10-28 12:40:05 -0300

    Configuração inicial do projeto Spring Boot

---
+++ src/test/java/com/Business_Intelligence/bi_system_backend/BiSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Business_Intelligence.bi_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
